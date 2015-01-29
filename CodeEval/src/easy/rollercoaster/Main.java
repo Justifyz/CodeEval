6604efc0c6eda8f8b5ceba824c8f834f077ae0d3
JONATHAN BASDEN <jonathan.w.basden@jpmchase.com>
2015-01-29 14:24:11 -0600

    adding rollercoaster problem under easy

---
+++ CodeEval/src/easy/rollercoaster/Main.java
@@ -0,0 +1,26 @@
+package easy.rollercoaster;
+import java.io.*;
+public class Main {
+    public static void main (String[] args) throws IOException {
+        File file = new File(args[0]);
+        BufferedReader buffer = new BufferedReader(new FileReader(file));
+        String line;
+        while ((line = buffer.readLine()) != null) {
+        	char[] input = line.trim().toCharArray();
+        	boolean rotate = false;
+        	for(int i = 0; i < input.length; i++) {
+        		if(Character.isLetter(input[i])) {
+        			if(!rotate) {
+        				input[i] = Character.toUpperCase(input[i]);
+        				rotate = true;
+        			}
+        			else {
+        				input[i] = Character.toLowerCase(input[i]);
+        				rotate = false;
+        			}      			
+        		}
+        	}
+        	System.out.println(String.valueOf(input));
+        }
+    }
+}
