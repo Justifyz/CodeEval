d5574f54ef2698591bf6baec905b8488b5caf5c1
JONATHAN BASDEN <jonathan.w.basden@jpmchase.com>
2015-02-02 16:36:44 -0600

    adding problem under easy.

--- CodeEval/src/easy/withoutrepetitions/Main.java
+++ CodeEval/src/easy/withoutrepetitions/Main.java
@@ -2,18 +2,16 @@ package easy.withoutrepetitions;
 import java.io.*;
 public class Main {
     public static void main (String[] args) throws IOException {
-        File file = new File(args[0]);
+        File file = new File("input.txt");
         BufferedReader buffer = new BufferedReader(new FileReader(file));
         String line;
         while ((line = buffer.readLine()) != null) {
         	StringBuilder sb = new StringBuilder();
             char[] input = line.trim().toCharArray();
-            char previousChar = input[0];
-            sb.append(previousChar);
+            sb.append(input[0]);
             for(int i = 1; i < input.length; i++) {
-            	if(previousChar != input[i]) {
-            		previousChar = input[i];
-            		sb.append(previousChar);
+            	if(input[i] != input[i-1]) {
+            		sb.append(input[i]);
             	}
             }
             System.out.println(sb.toString());
