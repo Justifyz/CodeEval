c0adfa43584667cc9e0f3a6e73c0a955906dfc84
JONATHAN BASDEN <jonathan.w.basden@jpmchase.com>
2015-02-02 16:39:51 -0600

    adding problem under easy.

--- CodeEval/src/easy/withoutrepetitions/Main.java
+++ CodeEval/src/easy/withoutrepetitions/Main.java
@@ -2,7 +2,7 @@ package easy.withoutrepetitions;
 import java.io.*;
 public class Main {
     public static void main (String[] args) throws IOException {
-        File file = new File("input.txt");
+        File file = new File(args[0]);
         BufferedReader buffer = new BufferedReader(new FileReader(file));
         String line;
         while ((line = buffer.readLine()) != null) {
