5d23fad886af44905360b62c1bc7dc14d18ce36f
JONATHAN BASDEN <jonathan.w.basden@jpmchase.com>
2015-02-05 11:34:24 -0600

    adding bigdigits under easy.

---
+++ CodeEval/src/easy/bigdigits/Main.java
@@ -0,0 +1,90 @@
+package easy.bigdigits;
+import java.io.*;
+import java.util.ArrayList;
+public class Main {
+    public static void main (String[] args) throws IOException {
+        File file = new File("input.txt");
+        BufferedReader buffer = new BufferedReader(new FileReader(file));
+        String line;
+        String[][] bigDigits = 
+        { 
+          {"-**-",
+           "*--*",
+           "*--*",
+           "*--*",
+           "-**-",
+           "----"},
+          {"--*-",
+           "-**-",
+           "--*-",
+           "--*-",
+           "-***",
+           "----"},
+          {"***-",
+           "---*",
+           "-**-",
+           "*---",
+           "****",
+           "----"},
+          {"***-",
+           "---*",
+           "-**-",
+           "---*",
+           "***-",
+           "----"},
+          {"-*--",
+           "*--*",
+           "****",
+           "---*",
+           "---*",
+           "----"},
+          {"****",
+           "*---",
+           "***-",
+           "---*",
+           "***-",
+           "----"},
+          {"-**-",
+           "*---",
+           "***-",
+           "*--*",
+           "-**-",
+           "----"},
+          {"****",
+           "---*",
+           "--*-",
+           "-*--",
+           "-*--",
+           "----"},
+          {"-**-",
+           "*--*",
+           "-**-",
+           "*--*",
+           "-**-",
+           "----"},
+          {"-**-",
+           "*--*",
+           "-***",
+           "---*",
+           "-**-",
+           "----"}
+        };
+        while ((line = buffer.readLine()) != null) {
+        	char[] input = line.trim().toCharArray();
+        	ArrayList<Integer> list = new ArrayList<Integer>();
+        	for(int i = 0; i < input.length; i++) {
+        		if(Character.isDigit(input[i])) {
+        			list.add(Character.getNumericValue(input[i]));
+        		}
+        	}
+        	StringBuilder sb = new StringBuilder();
+        	for(int i = 0; i < 6; i++) {
+        		for(int j = 0; j < list.size(); j++) {
+        			sb.append(bigDigits[list.get(j)][i]+"-");
+        		}
+        		sb.append("\n");
+        	}
+        	System.out.print(sb.toString());
+        }
+    }
+}
